c6bfd2ef5ca1538da057a9dede5d6029a8942e72
mtahasylmz <mustah135@gmail.com>
2025-03-19 04:35:28 +0300

    works with the initial axios example

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { isDocUrl } from './doc-check-open';
+import { isDocUrl } from './doc-check.js';
 
-async function main() {
+async function main() : Promise<void>{
   const result1 = await isDocUrl("https://github.com/axios/axios");
   const result2 = await isDocUrl("https://github.com/axios/axios-docs");
   
-  console.log(`Example 1: ${result1}`);
-  console.log(`Example 2: ${result2}`);
+  console.log(`Example 1: ${result1.isValid} and the reason is ${result1.reason}`);
+  console.log(`Example 2: ${result2.isValid} and the reason is ${result2.reason}`);
 }
 
 main().catch(console.error);
